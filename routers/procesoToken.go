275489d53eeaf59b546396b528d977cf965acea1
Sergio Paez <52540239+SergioPaez96@users.noreply.github.com>
2020-10-12 20:04:11 -0300

    verPerfil

---
+++ routers/procesoToken.go
@@ -0,0 +1,47 @@
+package routers
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/SergioPaez96/twitter_react/bd"
+	"github.com/SergioPaez96/twitter_react/models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+/*Email valor de Email usado en todos los EndPoints*/
+var Email string
+
+/*IDUsuario es el ID devuelto del modelo, que se usará en todos los EndPoints*/
+var IDUsuario string
+
+/*ProcesoToken proceso token para extraer sus valores*/
+func ProcesoToken(tk string) (*models.Claim, bool, string, error) {
+	miClave := []byte("SergioPaez96")
+	claims := &models.Claim{}
+
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return claims, false, string(""), errors.New("formato de token invalido")
+	}
+
+	tk = strings.TrimSpace(splitToken[1])
+
+	tkn, err := jwt.ParseWithClaims(tk, claims, func(token *jwt.Token) (interface{}, error) {
+		return miClave, nil
+	})
+
+	if err == nil {
+		_, encontrado, _ := bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado == true {
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+		}
+		return claims, encontrado, IDUsuario, nil
+	}
+	if !tkn.Valid {
+		return claims, false, string(""), errors.New("token invalido")
+	}
+
+	return claims, false, string(""), err
+}
